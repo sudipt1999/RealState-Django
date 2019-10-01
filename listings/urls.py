07cc26ac046d174fd6b0776ece8994dbca64631b
sudipt1999 <sudiptdabral2991999@gmail.com>
2019-10-01 19:48:46 +0530

    initial commit

---
+++ listings/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+from pages import views as v
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', views.index, name="listings"),
+    path('<int:listing_id>', views.listing, name="listing"),
+    path('search', views.search, name="search"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
